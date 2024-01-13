6d72cc8b3d637d2f200630b863468484b9735b39
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2024-01-13 22:54:55 +0530

    v5

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -4,9 +4,16 @@ MODDIR="${0%/*}"
 MODNAME="${MODDIR##*/}"
 MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
 
+if [ "$(magisk -V)" -lt 26302 ] || [ "$(/data/adb/ksud -V)" -lt 10818 ]; then
+  touch "$MODDIR/disable"
+fi
+
 if [ ! -e "$MAGISKTMP/.magisk/mirror/sepolicy.rules/$MODNAME/sepolicy.rule" ] && [ ! -e "$MAGISKTMP/.magisk/sepolicy.rules/$MODNAME/sepolicy.rule" ]; then
     magiskpolicy --live --apply "$MODDIR/sepolicy.rule"
+    ksud sepolicy apply "$MODDIR/sepolicy.rule"
 fi
+
+ksud sepolicy apply "$MODDIR/sepolicy.rule"
 
 . "$MODDIR/resetprop.sh"
 
@@ -16,31 +23,41 @@ fi
 #    userdebug -> user
 #    test-keys -> release-keys
 
-replace_value_resetprop ro.build.fingerprint userdebug user
-replace_value_resetprop ro.build.fingerprint "aosp_" ""
-replace_value_resetprop ro.build.fingerprint "lineage_" ""
-replace_value_resetprop ro.build.fingerprint test-keys release-keys
-replace_value_resetprop ro.build.description userdebug user
+# Fix Lineage and Debugging props
 replace_value_resetprop ro.build.description "aosp_" ""
-replace_value_resetprop ro.build.description "lineage_" ""
-replace_value_resetprop ro.build.description test-keys release-keys
+replace_value_resetprop ro.build.fingerprint "aosp_" ""
 replace_value_resetprop ro.build.flavor "aosp_" ""
+replace_value_resetprop ro.product.bootimage.name "aosp_" ""
 replace_value_resetprop ro.product.name "aosp_" ""
-replace_value_resetprop ro.build.flavor "lineage_" ""
-replace_value_resetprop ro.product.name "lineage_" ""
-replace_value_resetprop ro.build.flavor "userdebug" ""
 
-for prefix in system vendor system_ext product oem odm vendor_dlkm odm_dlkm; do
-    check_resetprop ro.${prefix}.build.type user
+replace_value_resetprop ro.build.description "lineage_" ""
+replace_value_resetprop ro.build.fingerprint "lineage_" ""
+replace_value_resetprop ro.build.flavor "lineage_" ""
+replace_value_resetprop ro.product.bootimage.name "lineage_" ""
+replace_value_resetprop ro.product.name "lineage_" ""
+
+replace_value_resetprop ro.build.description test-keys release-keys
+replace_value_resetprop ro.build.description userdebug user
+replace_value_resetprop ro.build.fingerprint test-keys release-keys
+replace_value_resetprop ro.build.fingerprint userdebug user
+replace_value_resetprop ro.build.flavor userdebug user
+
+for prefix in system vendor system_ext product oem odm vendor_dlkm odm_dlkm bootimage; do
     check_resetprop ro.${prefix}.build.tags release-keys
-    replace_value_resetprop ro.${prefix}.build.fingerprint userdebug user
-    replace_value_resetprop ro.${prefix}.build.fingerprint "aosp_" ""
-    replace_value_resetprop ro.${prefix}.build.fingerprint "lineage_" ""
-    replace_value_resetprop ro.${prefix}.build.fingerprint test-keys release-keys
-    replace_value_resetprop ro.${prefix}.build.description userdebug user
-    replace_value_resetprop ro.${prefix}.build.description "aosp_" ""
-    replace_value_resetprop ro.${prefix}.build.description "lineage_" ""
+    check_resetprop ro.${prefix}.build.type user
     replace_value_resetprop ro.${prefix}.build.description test-keys release-keys
+    replace_value_resetprop ro.${prefix}.build.description userdebug user
+    replace_value_resetprop ro.${prefix}.build.fingerprint test-keys release-keys
+    replace_value_resetprop ro.${prefix}.build.fingerprint userdebug user
+    replace_value_resetprop ro.${prefix}.build.description "aosp_" ""
+    replace_value_resetprop ro.${prefix}.build.fingerprint "aosp_" ""
     replace_value_resetprop ro.product.${prefix}.name "aosp_" ""
+    replace_value_resetprop ro.${prefix}.build.description "lineage_" ""
+    replace_value_resetprop ro.${prefix}.build.fingerprint "lineage_" ""
     replace_value_resetprop ro.product.${prefix}.name "lineage_" ""
+  # check_resetprop ro.${prefix}.build.date.utc $(date +"%s")
 done
+
+# check_resetprop ro.build.date.utc $(date +"%s")
+# check_resetprop ro.build.version.security_patch $(date +2023-%m-%d)
+# check_resetprop ro.vendor.build.security_patch $(date +2023-%m-%d)
--- service.sh
+++ service.sh
@@ -1,6 +1,7 @@
 MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
 MODPATH="${0%/*}"
 
+
 # Use Magisk Delta feature to dynamic patch prop
 
 [ -d "$MAGISKTMP/.magisk/mirror/early-mount/initrc.d" ] && cp -Tf "$MODPATH/oem.rc" "$MAGISKTMP/.magisk/mirror/early-mount/initrc.d/oem.rc"
@@ -8,7 +9,7 @@ MODPATH="${0%/*}"
 . "$MODPATH/resetprop.sh"
 
 # Hiding SELinux | Use toybox to protect *stat* access time reading
-if [[ "$(toybox cat /sys/fs/selinux/enforce)" == "0" ]]; then
+if [ "$(toybox cat /sys/fs/selinux/enforce)" == "0" ]; then
     chmod 640 /sys/fs/selinux/enforce
     chmod 440 /sys/fs/selinux/policy
 fi
@@ -19,48 +20,85 @@ done
 
 # these props should be set after boot completed to avoid breaking some device features
 
-check_resetprop ro.boot.vbmeta.device_state locked
-check_resetprop ro.boot.verifiedbootstate green
+# Avoid breaking Realme fingerprint scanners
 check_resetprop ro.boot.flash.locked 1
+
+# Avoid breaking Oppo fingerprint scanners
+check_resetprop ro.boot.vbmeta.device_state locked
+
+# Avoid breaking OnePlus display modes/fingerprint scanners
+check_resetprop vendor.boot.verifiedbootstate green
+
+# Avoid breaking OnePlus/Oppo display fingerprint scanners on OOS/ColorOS 12+
+check_resetprop ro.boot.verifiedbootstate green
 check_resetprop ro.boot.veritymode enforcing
+check_resetprop vendor.boot.vbmeta.device_state locked
+
+# Samsung
 check_resetprop ro.boot.warranty_bit 0
 check_resetprop ro.warranty_bit 0
+check_resetprop ro.vendor.boot.warranty_bit 0
+check_resetprop ro.vendor.warranty_bit 0
+
+# OnePlus
+check_resetprop ro.is_ever_orange 0
+    
 check_resetprop ro.debuggable 0
 check_resetprop ro.secure 1
+check_resetprop ro.adb.secure 1
+
 check_resetprop ro.secureboot.devicelock 1
 check_resetprop ro.secureboot.lockstate locked
+
+# RootBeer, Microsoft
 check_resetprop ro.build.type user
 check_resetprop ro.build.keys release-keys
 check_resetprop ro.build.tags release-keys
-check_resetprop ro.vendor.boot.warranty_bit 0
-check_resetprop ro.vendor.warranty_bit 0
-check_resetprop vendor.boot.vbmeta.device_state locked
-check_resetprop vendor.boot.verifiedbootstate green
+
+# makes bank apps and Google Pay happy
 check_resetprop sys.oem_unlock_allowed 0
 check_resetprop ro.oem_unlock_supported 0
+
+# Init.rc
 check_resetprop init.svc.flash_recovery stopped
+
+# Realme
 check_resetprop ro.boot.realmebootstate green
 check_resetprop ro.boot.realme.lockstate 1
 
 # fake encryption
 check_resetprop ro.crypto.state encrypted
 
+# Disable Lsposed logs
+resetprop -n persist.log.tag.LSPosed S
+resetprop -n persist.log.tag.LSPosed-Bridge S
+
+# Fix Native Bridge Detection 
+# resetprop --delete ro.dalvik.vm.native.bridge
+
+# Hide that we booted from recovery when magisk is in recovery mode
 maybe_resetprop ro.bootmode recovery unknown
 maybe_resetprop ro.boot.bootmode recovery unknown
 maybe_resetprop ro.boot.mode recovery unknown
 maybe_resetprop vendor.bootmode recovery unknown
 maybe_resetprop vendor.boot.bootmode recovery unknown
 maybe_resetprop vendor.boot.mode recovery unknown
+
+# MIUI cross-region flash
 maybe_resetprop ro.boot.hwc CN GLOBAL
 maybe_resetprop ro.boot.hwcountry China GLOBAL
-selinux="$(resetprop ro.build.selinux)"
-[ -z "$selinux" ] || resetprop --delete ro.build.selinux
+
+# SELinux
+if [ -n "$(resetprop ro.build.selinux)" ]; then
+    resetprop --delete ro.build.selinux
+fi
 
 for prefix in system vendor system_ext product oem odm vendor_dlkm odm_dlkm; do
     check_resetprop ro.${prefix}.build.type user
     check_resetprop ro.${prefix}.build.tags release-keys
 done
 
+# Avoid breaking encryption, set shipping level to 32 for devices >=33 to allow for software attestation
 if [[ "$(resetprop -v ro.product.first_api_level)" -ge 33 ]]; then
     resetprop -v -n ro.product.first_api_level 32
 fi
@@ -70,3 +108,11 @@ chmod 0440 /proc/cmdline
 
 # Restrict permissions to socket file to hide Magisk & co.
 chmod 0440 /proc/net/unix
+
+# Hide Addon.d
+chmod 0750 /system/addon.d
+
+# Fix Restrictions on non-SDK interface
+settings delete global hidden_api_policy
+settings delete global hidden_api_policy_pre_p_apps
+settings delete global hidden_api_policy_p_apps
